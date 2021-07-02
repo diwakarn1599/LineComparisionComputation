0dc0e14d8bcfb3f0f191071fb14a57bbe1d71e4a
Diwakar <diwakarn1599@gmail.com>
2021-07-02 20:05:29 +0530

    [Diwakar] Updated Usecase 4 to compute using oops concepts

---
+++ LineComparisionComputation/LineComparision.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisionComputation
+{
+    public class LineComparision
+    {
+        private int x1, x2, y1, y2;
+        private double lineLength;
+        //static list for storing lengths
+        public static List<double> lengths = new List<double>();
+        
+        //normal constructor
+        public LineComparision()
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Problem");
+        }
+        //parameterised constructor
+        public LineComparision(int a1, int b1, int a2,int b2)
+        {
+            this.x1 = a1;
+            this.y1 = b1;
+            this.x2 = a2;
+            this.y2 = b2;
+
+            calculateLength();
+
+        }
+
+        //method for calculating length
+        public void calculateLength()
+        {
+            
+            lineLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            
+            lengths.Add(lineLength);
+            //Console.WriteLine(lengths[lengths.Count - 1]);
+            //Console.WriteLine(lengths.Count);
+        }
+        
+        //method for compare lines
+        public void compareLines()
+        {
+            //Console.WriteLine(lengths.Count);
+            //printing length of each line
+            Console.WriteLine($"Line 1 Length: {lengths[0]}");
+            Console.WriteLine($"Line 2 Length: {lengths[1]}");
+
+            //uc1 using equals method
+            Console.WriteLine("Checking Equality of line using Equals Method");
+            String result = lengths[0].Equals(lengths[1]) ? "Line one is equal to line Two" : "Line one is not equal to line Two";
+            Console.WriteLine(result);
+
+            //uc2 using compare to method
+            Console.WriteLine("Using CompareTo method check equality");
+            int res = lengths[0].CompareTo(lengths[1]);
+            if (res < 0)
+            {
+                Console.WriteLine("Line 1 is less than line 2");
+            }
+            else if (res == 0)
+            {
+                Console.WriteLine("Line 1 is equal to line 2");
+            }
+            else
+            {
+                Console.WriteLine("Line 1 is greater than line 2");
+            }
+        }
+
+
+    }
+}
--- LineComparisionComputation/Program.cs
+++ LineComparisionComputation/Program.cs
@@ -4,54 +4,47 @@ namespace LineComparisionComputation
 {
     class Program
     {
+        //intialising object for line comparision
+        LineComparision lc;
+
+        //method for initialize constructor
+        private  void computeLength(int x1, int y1,int  x2,int y2)
+        {
+            
+            lc = new LineComparision(x1,y1,x2,y2);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Computation Problem");
-            //initialising coordinates
-            int x1, x2, y1, y2,p1,p2,q1,q2;
-            double lineOneLength,lineTwoLength;
-            //getting inputs from user for first line
-            Console.WriteLine("Enter X-coordinate of First Point of First Line:");
-            x1 = Convert.ToInt32(Console.ReadLine());//reading input and convert it to integer
-            Console.WriteLine("Enter Y-coordinate of First Point First Line:");
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter X-coordinate of Second Point First Line:");
-            x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Y-coordinate of Second Point First Line:");
-            y2 = Convert.ToInt32(Console.ReadLine());
-
-            //getting inputs from user for Second line 
-            Console.WriteLine("Enter X-coordinate of First Point of Second Line:");
-            p1 = Convert.ToInt32(Console.ReadLine());//reading input and convert it to integer
-
-            Console.WriteLine("Enter Y-coordinate of First Point Second Line:");
-            q1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter X-coordinate of Second Point Second Line:");
-            p2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter Y-coordinate of Second Point Second Line:");
-            q2 = Convert.ToInt32(Console.ReadLine());
-
-            //formula for calculate length
-            lineOneLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            lineTwoLength = Math.Sqrt(Math.Pow(p2 - p1, 2) + Math.Pow(q2 - q1, 2));
-            //Checking if line1 equals line 2 by Equals() and ternary operator 
-            //String result = lineOneLength.Equals(lineTwoLength) ? "Line one is equal to line Two" : "Line one is not equal to line Two";
-            //Console.WriteLine(result);
-            int result = lineOneLength.CompareTo(lineTwoLength);
-            if (result < 0)
+            
+            
+            int x1, x2, y1, y2;
+            //object for program class
+            Program pg = new Program();
+            for(int i = 1; i <= 2; i++)
             {
-                Console.WriteLine("Line 1 is less than line 2");
-            }
-            else if(result == 0)
-            {
-                Console.WriteLine("Line 1 is equal to line 2");
-            }
-            else
-            {
-                Console.WriteLine("Line 1 is greater than line 2");
-            }
+                
+                Console.WriteLine($"Enter X-coordinate of First Point Line {i}:");
+                x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter Y-coordinate of First Point Line {i}:");
+                y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter X-coordinate of Second Point Line {i}:");
+                x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter Y-coordinate of Second Point Line {i}:");
+                y2 = Convert.ToInt32(Console.ReadLine());
+
+                //calling method to invoke constructor
+                pg.computeLength(x1, y1, x2, y2);
+             }
+
+            //object for line comparision class
+            LineComparision lcc = new LineComparision();
+
+            //calling non-static method
+            lcc.compareLines();
+            
+            
+
+           
 
 
 
